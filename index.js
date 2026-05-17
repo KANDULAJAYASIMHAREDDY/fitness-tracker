a3ba8c43cd368a5ad963836108c3f150a48ed782
jayasimha <kjayasimhareddy123@gmail.com>
2026-05-17 16:28:05 +0530

    fix: allow all cors origins

--- index.js
+++ index.js
@@ -8,19 +8,7 @@ const app=express();
 const port=process.env.PORT||3333;
 
 //(middlewares)
-const allowedOrigins = [
-  'http://localhost:5173',
-  'http://localhost:3000',
-  process.env.FRONTEND_URL,
-].filter(Boolean);
-
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) return callback(null, true);
-    callback(new Error('Not allowed by CORS'));
-  },
-  credentials: true
-}));
+app.use(cors());
 app.use(express.json())
 
 
